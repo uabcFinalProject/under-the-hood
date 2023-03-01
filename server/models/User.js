9f6649bebf425c8f7956147b3974f1d495527649
maggierdelaney <111933814+maggierdelaney@users.noreply.github.com>
2023-02-28 21:34:07 -0600

    Added User model

---
+++ server/models/User.js
@@ -0,0 +1,62 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const vehicleSchema = require('./Vehicle');
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must use a valid email address'],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    phoneNumber: {
+        type: Int,
+        required: true,
+    },
+    vehicles: [vehicleSchema],
+  },
+  // set this to use virtual below
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+// hash user password
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// custom method to compare and validate password for logging in
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+// when we query a user, we'll also get another field called `bookCount` with the number of saved books we have
+userSchema.virtual('bookCount').get(function () {
+  return this.savedBooks.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
