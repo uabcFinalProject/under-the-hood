bd2252907f8e9aa27832ba6acd9d054dea752136
Isabella <isabellapierce93@gmail.com>
2023-03-04 15:02:24 -0700

    Updated app.js file and created nomatch file.

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,90 @@
 import React from 'react';
 import './App.css';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
 import Nav from './components/nav/nav';
+import Login from './components/login/login';
+import Signup from './components/signUpForm/signup';
+import Home from './pages/home';
+import History from './pages/history';
+import Profile from './pages/profile';
+import Reminder from './pages/reminders';
+import Tools from './pages/tools';
+import NoMatch from './pages/nomatch';
+
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      
-      <Nav>
-      </Nav>
-
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          {/* <StoreProvider> */}
+            <Nav />
+            <Routes>
+              <Route 
+                path="/" 
+                element={<Home />} 
+              />
+              <Route 
+                path="/login" 
+                element={<Login />} 
+              />
+              <Route 
+                path="/signup" 
+                element={<Signup />} 
+              />
+              <Route 
+                path="/history" 
+                element={<History />} 
+              />
+              <Route 
+                path="/profile" 
+                element={<Profile />} 
+              />
+               <Route 
+                path="/reminder" 
+                element={<Reminder />} 
+              />
+               <Route 
+                path="/tools" 
+                element={<Tools />} 
+              />
+              <Route 
+                path="*"
+                element={<NoMatch />} 
+              />
+            </Routes>
+          {/* </StoreProvider> */}
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
