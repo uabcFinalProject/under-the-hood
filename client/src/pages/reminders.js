28cd9260c6f1126e25c1cca11ea1c0751c2f7997
Isabella <isabellapierce93@gmail.com>
2023-03-06 21:23:05 -0700

    Menu links are working, added reminders page and updated home.

--- client/src/pages/reminders.js
+++ client/src/pages/reminders.js
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Input, List } from 'antd';
 
-function Reminders () {
-    return (
-        <div></div>
-    );
-}
+const Reminder = () => {
+  const [reminders, setReminders] = useState([]);
+  const [inputValue, setInputValue] = useState('');
 
-export default Reminders
+  const handleAddReminder = () => {
+    if (inputValue.trim() !== '') {
+      setReminders([...reminders, { text: inputValue, completed: false }]);
+      setInputValue('');
+    }
+  };
+
+  const handleCompleteReminder = (index) => {
+    const newReminders = [...reminders];
+    newReminders[index].completed = true;
+    setReminders(newReminders);
+  };
+
+  return (
+    <div>
+      <Input
+        placeholder="Enter a reminder"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        onPressEnter={handleAddReminder}
+      />
+      <Button type="primary" onClick={handleAddReminder}>
+        Add
+      </Button>
+      <List
+        style={{ marginTop: 16 }}
+        bordered
+        dataSource={reminders}
+        renderItem={(item, index) => (
+          <List.Item>
+            <span
+              style={{
+                textDecoration: item.completed ? 'line-through' : 'none',
+              }}
+            >
+              {item.text}
+            </span>
+            {!item.completed && (
+              <Button
+                type="primary"
+                style={{ marginLeft: 8 }}
+                onClick={() => handleCompleteReminder(index)}
+              >
+                Complete
+              </Button>
+            )}
+          </List.Item>
+        )}
+      />
+    </div>
+  );
+};
+
+export default Reminder;
